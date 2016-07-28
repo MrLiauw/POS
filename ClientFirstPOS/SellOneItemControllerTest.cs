3c75aedaa134087f7502ca4fe6dfb1def227d2ba
MrLiauw <maryantoliauw@gmail.com>
2016-07-28 18:38:55 +0800

    we now gracefully handled barcode which we do not find the price

--- ClientFirstPOS/SellOneItemControllerTest.cs
+++ ClientFirstPOS/SellOneItemControllerTest.cs
@@ -17,6 +17,18 @@ namespace ClientFirstPOS
             saleController.onBarcode("12345");
             mockDisplay.Verify(d=>d.DisplayPrice(irrelevantPrice), Times.Once);
         }
+
+        [Test]
+        public void productNotFound()
+        {
+            Mock<IDisplay> mockDisplay = new Mock<IDisplay>();
+            Mock<ICatalog> mockCatalog = new Mock<ICatalog>();
+            mockCatalog.Setup(c => c.FindPrice("::product not found::")).Returns((Price) null);
+
+            SaleController saleController = new SaleController(mockDisplay.Object, mockCatalog.Object);
+            saleController.onBarcode("::product not found::");
+            mockDisplay.Verify(d => d.DisplayProductNotFoundMessage(It.IsRegex(@".\:{1}product not found\:{1}.")), Times.Once);
+        }
     }
 
     public class SaleController
@@ -32,13 +44,18 @@ namespace ClientFirstPOS
 
         public void onBarcode(string barcode)
         {
-            display.DisplayPrice(catalog.FindPrice(barcode));
+            Price price = catalog.FindPrice(barcode);
+            if(price == null)
+                display.DisplayProductNotFoundMessage(barcode);
+            else
+                display.DisplayPrice(price);
         }
     }
 
     public interface IDisplay
     {
         void DisplayPrice(Price cents);
+        void DisplayProductNotFoundMessage(string message);
     }
 
     public interface ICatalog

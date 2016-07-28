2845e5f9809a192aea76365c33940931e71f9475
MrLiauw <maryantoliauw@gmail.com>
2016-07-28 18:04:31 +0800

    we can sell an item with any price, as long as we have the price

--- ClientFirstPOS/SellOneItemControllerTest.cs
+++ ClientFirstPOS/SellOneItemControllerTest.cs
@@ -1,13 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using NUnit.Framework;
 
 namespace ClientFirstPOS
 {
     class SellOneItemControllerTest
     {
+        [Test]
+        public void productFound()
+        {
+            Mock<IDisplay> mockDisplay = new Mock<IDisplay>();
+            Mock<ICatalog> mockCatalog = new Mock<ICatalog>();
+            Price irrelevantPrice = Price.cents(795);
+            mockCatalog.Setup(c => c.FindPrice(It.IsAny<string>())).Returns(irrelevantPrice);
 
+            SaleController saleController = new SaleController(mockDisplay.Object, mockCatalog.Object);
+            saleController.onBarcode("12345");
+            mockDisplay.Verify(d=>d.DisplayPrice(irrelevantPrice), Times.Once);
+        }
+    }
+
+    public class SaleController
+    {
+        private IDisplay display;
+        private ICatalog catalog;
+
+        public SaleController(IDisplay display, ICatalog catalog)
+        {
+            this.display = display;
+            this.catalog = catalog;
+        }
+
+        public void onBarcode(string barcode)
+        {
+            display.DisplayPrice(catalog.FindPrice(barcode));
+        }
+    }
+
+    public interface IDisplay
+    {
+        void DisplayPrice(Price cents);
+    }
+
+    public interface ICatalog
+    {
+        Price FindPrice(string barcode);
+    }
+
+    public class Price
+    {
+        public static Price cents(int centsValue)
+        {
+            return new Price();
+        }
     }
 }

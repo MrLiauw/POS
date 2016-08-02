3ea44632e46fa50f339c4f768069631d68512a8e
MrLiauw <maryantoliauw@gmail.com>
2016-08-02 15:23:20 +0800

    we correctly signal that we couldn't find price in the inmemorycatalog

---
+++ ClientFirstPOS/FindPriceInMemoryCatalogTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ClientFirstPOS
+{
+    public class FindPriceInMemoryCatalogTest
+    {
+        [Test]
+        public void ProductFound()
+        {
+            Price foundPrice = Price.cents(1250);
+            InMemoryCatalog catalog = new InMemoryCatalog(new Dictionary<string, Price>(){
+                {
+                    "12345", foundPrice
+                }});
+
+            Assert.AreEqual(foundPrice, catalog.FindPrice("12345"));
+
+        }
+
+        [Test]
+        public void ProductNotFound()
+        {
+            InMemoryCatalog catalog = new InMemoryCatalog(new Dictionary<string, Price>());
+
+            Assert.AreEqual(null, catalog.FindPrice("12345"));
+        }
+    }
+}

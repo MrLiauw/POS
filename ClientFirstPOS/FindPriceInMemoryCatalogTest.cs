c33b0850a4c3745cee9e90f6bfbc124d65ad4dd1
MrLiauw <maryantoliauw@gmail.com>
2016-08-02 15:36:51 +0800

    Confirmed InmemoryCatalog respect contract

--- ClientFirstPOS/FindPriceInMemoryCatalogTest.cs
+++ ClientFirstPOS/FindPriceInMemoryCatalogTest.cs
@@ -9,21 +9,29 @@ namespace ClientFirstPOS
         public void ProductFound()
         {
             Price foundPrice = Price.cents(1250);
-            InMemoryCatalog catalog = new InMemoryCatalog(new Dictionary<string, Price>(){
-                {
-                    "12345", foundPrice
-                }});
+            ICatalog catalog = CatalogWith("12345", foundPrice);
 
             Assert.AreEqual(foundPrice, catalog.FindPrice("12345"));
 
         }
 
+        private static ICatalog CatalogWith(string barcode, Price price)
+        {
+            return new InMemoryCatalog(new Dictionary<string, Price>(){
+                {
+                    barcode, price
+                }});
+        }
+
         [Test]
         public void ProductNotFound()
         {
-            InMemoryCatalog catalog = new InMemoryCatalog(new Dictionary<string, Price>());
+            Assert.AreEqual(null, CatalogWithout("12345").FindPrice("12345"));
+        }
 
-            Assert.AreEqual(null, catalog.FindPrice("12345"));
+        private static ICatalog CatalogWithout(string price)
+        {
+            return new InMemoryCatalog(new Dictionary<string, Price>());
         }
     }
 }

7f9afe5329df73c2d9a30ad43e539be1270250e7
MrLiauw <maryantoliauw@gmail.com>
2016-08-16 17:21:53 +0800

    making similar code even more similar

---
+++ ClientFirstPOS/ConsoleDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClientFirstPOS
+{
+    public class ConsoleDisplay
+    {
+        public void displayProductNotFoundMessage(string barcodeNotFound)
+        {
+            Console.WriteLine(
+                string.Format("Product not found for {0}", barcodeNotFound));
+        }
+
+        internal void displayEmptyBarcodeMessage()
+        {
+            Console.WriteLine(string.Format("Scanning error: empty barcode"));
+        }
+
+        public void displayPrice(Price price)
+        {
+            decimal priceInDollars = price.PriceInDollars();
+            Console.WriteLine(
+                string.Format("${0}", priceInDollars.ToString("#,##0.00")));
+        }
+    }
+}

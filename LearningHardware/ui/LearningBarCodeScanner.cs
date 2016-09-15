603618e9762ddf050e0a8953e18aa6beb1f395f1
MrLiauw <maryantoliauw@gmail.com>
2016-09-15 17:01:33 +0800

    ensure barcode scanner work as keyboard

---
+++ LearningHardware/ui/LearningBarCodeScanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClientFirstPOS.ui
+{
+    class LearningBarCodeScanner
+    {
+        public static void Main(string[] args)
+        {
+            string line;
+            do
+            {
+                line = Console.ReadLine();
+                Console.WriteLine(line);
+            } while (!string.IsNullOrEmpty(line));
+        }
+    }
+}

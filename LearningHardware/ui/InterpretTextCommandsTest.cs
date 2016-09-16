8b864db1d8bd5001e64cb448e261e28b55a6cfe3
MrLiauw <maryantoliauw@gmail.com>
2016-09-16 17:25:13 +0800

    We can now handle empty barcode

---
+++ LearningHardware/ui/InterpretTextCommandsTest.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace LearningHardware.ui
+{
+    class InterpretTextCommandsTest
+    {
+        [Test]
+        public void zero()
+        {
+            Mock<BarcodeScannedListener> barcodeScannedListener = new Mock<BarcodeScannedListener>();
+
+            barcodeScannedListener.Verify(x=>x.onBarcode(""), Times.Never());
+
+            new TextCommandInterpreter().process("");
+        }
+    }
+
+    public interface BarcodeScannedListener
+    {
+        void onBarcode(string barcode);
+    }
+
+    public class TextCommandInterpreter
+    {
+        public void process(string text)
+        {
+        }
+    }
+}
